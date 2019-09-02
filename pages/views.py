e4bdb275b303af36356bfa49cce95adc645e3252
abuzarrizvi <syedabuzar12@gmail.com>
2019-09-02 14:55:20 +0500

    Django Templates
    Django Templates, all views previously done by HttpResponse are now implemented using Django Template concepts.

--- pages/views.py
+++ pages/views.py
@@ -3,17 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-
+	#return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+	return render(request,"home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	print(request.user)
-	return HttpResponse("<h1>Contact Page</h1>") # string of HTML code
-
+	#print(request.user)
+	#return HttpResponse("<h1>Contact Page</h1>") # string of HTML code
+	return render(request,"contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About page</h1>") # string of HTML code
-
+	#return HttpResponse("<h1>About page</h1>") # string of HTML code
+	return render(request,"about.html", {})
 
 def social_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Social Page</h1>") # string of HTML code
+	#return HttpResponse("<h1>Social Page</h1>") # string of HTML code
+	return render(request,"social.html", {})
