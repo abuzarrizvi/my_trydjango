70cdff6894941e2f45485014b978d2a4319dec7c
abuzarrizvi <syedabuzar12@gmail.com>
2019-09-02 14:43:01 +0500

    URL Routing and Requests. HttpResponse
    some more views are created. like contact, about and social fro understanding the URL Routing.
    its all done by HttpResponse

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
 	return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+
+def contact_view(request, *args, **kwargs):
+	print(request.user)
+	return HttpResponse("<h1>Contact Page</h1>") # string of HTML code
+
+
+def about_view(request, *args, **kwargs):
+	return HttpResponse("<h1>About page</h1>") # string of HTML code
+
+
+def social_view(request, *args, **kwargs):
+	return HttpResponse("<h1>Social Page</h1>") # string of HTML code
