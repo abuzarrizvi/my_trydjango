f68c8f24d5aa4b21f7b55bbfb146b33a3bc4bbde
abuzarrizvi <syedabuzar12@gmail.com>
2019-09-03 12:55:34 +0500

    Form Widgets
    Form Widgets, widgets are added onto the forms . next InshaAllah will do form validation

--- products/forms.py
+++ products/forms.py
@@ -12,6 +12,17 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-	title = forms.CharField()
-	description = forms.CharField()
-	price = forms.DecimalField()
+	title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Your Title"}))
+	description = forms.CharField(
+		required=False, 
+		widget=forms.Textarea(
+			attrs={
+				"class":"new-class-name two",
+				"rows": 20,
+				"placeholder":"Your description",
+				"id":"my-id-for-textarea",
+				"cols" :120
+			}
+			)
+		)
+	price = forms.DecimalField(initial=199.00)
