1de55258d3d7b127d094dc984c50b7840983364e
abuzarrizvi <syedabuzar12@gmail.com>
2019-09-03 10:29:38 +0500

    Render Data from the Database with a Model
    Render Data from the Database with a Model. product detail view is created in products application and product details are render on the html page.

--- my_trydjango/urls.py
+++ my_trydjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view, about_view, social_view
-
+from products.views import product_detail_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view),
+    path('product/', product_detail_view),
     path('about/', about_view),
     path('social/', social_view),
     path('admin/', admin.site.urls),
