75d27a1b03d5681ff922e2acf06bf612dd894e88
Syed Muhammad Abuzar Rizvi <syedabuzar12@gmail.com>
2019-09-16 13:35:38 +0500

    Dynamic Linking of URLs. as write function in model
    Dynamic Linking of URLs. as write function in model

--- products/models.py
+++ products/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
 	price 		= models.DecimalField(decimal_places=2, max_digits=1000)
 	summary     = models.TextField(blank=False, null= False)
 	featured 	= models.BooleanField(default=True) # null = True, default =True
+	
+	def get_absolute_url(self):
+		return (f"/products/{self.id}")
