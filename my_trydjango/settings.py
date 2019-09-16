1b0209ffb6d51dba5f5d063735f0c129a26c1bc5
Syed Muhammad Abuzar Rizvi <syedabuzar12@gmail.com>
2019-09-16 17:45:25 +0500

    Class Based Views - ListView
    Class Based Views - ListView

--- my_trydjango/settings.py
+++ my_trydjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #own application
     'products',
     'pages',
+	'blog',
 ]
 
 MIDDLEWARE = [
--- my_trydjango/urls.py
+++ my_trydjango/urls.py
@@ -19,6 +19,7 @@ from pages.views import home_view,contact_view, about_view, social_view
 
 urlpatterns = [
     path('products/', include('products.urls')),
+	path('blog/', include('blog.urls')),
 	path('', home_view, name='home'),
 	path('contact/', contact_view),
     
